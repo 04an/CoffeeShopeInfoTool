c9026e7c25c123884fb379d808156c16135fdca6
04an <olegdzhum@gmail.com>
2020-04-22 15:01:14 +0200

    add data access project to solution

---
+++ CoffeeHouse.DataAccess/CoffeeShopDP.cs
@@ -0,0 +1,16 @@
+﻿using CoffeeHouse.DataAccess.Model;
+using System.Collections.Generic;
+
+namespace CoffeeHouse.DataAccess
+{
+    public class CoffeeShopDP
+    {
+        public IEnumerable<CoffeeShop> LoadCoffeShops()
+        {
+            yield return new CoffeeShop { Location = "Warsaw", BeansInStockInKg = 191 };
+            yield return new CoffeeShop { Location = "Berlin", BeansInStockInKg = 91 };
+            yield return new CoffeeShop { Location = "Paris", BeansInStockInKg = 121 };
+
+        }
+    }
+}
---
+++ CoffeeHouse.DataAccess/Model/CoffeeShop.cs
@@ -0,0 +1,10 @@
+﻿namespace CoffeeHouse.DataAccess.Model
+{
+    public class CoffeeShop
+    {
+        public string Location { get; set; }
+        public int BeansInStockInKg { get; set; }
+
+    }
+}
+
