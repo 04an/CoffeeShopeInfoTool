cb12de9a6c8e01aa0f31361754b3b2ee434b2f01
04an <olegdzhum@gmail.com>
2020-04-22 15:01:36 +0200

    Implement help command

--- CoffeeHouse.ShopInfoTool/Program.cs
+++ CoffeeHouse.ShopInfoTool/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CoffeeHouse.DataAccess;
+using System;
 
 namespace CoffeeHouse.ShopInfoTool
 {
@@ -6,7 +7,27 @@ namespace CoffeeHouse.ShopInfoTool
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Coffee House - Shop info toll!");
+            Console.WriteLine("Coffee House - Shop info tool!");
+
+            Console.WriteLine("Write 'help' to list available commands");
+
+            var coffeeShopDP = new CoffeeShopDP();
+
+            while (true)
+            {
+                var line = Console.ReadLine();
+
+                var coffeeShops = coffeeShopDP.LoadCoffeShops();
+
+                if (string.Equals("help", line, StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("> Available coffee sho commands");
+                    foreach(var coffeeShop in coffeeShops)
+                    {
+                        Console.WriteLine("> " + coffeeShop.Location) ;
+                    }
+                }
+            }
         }
     }
 }
