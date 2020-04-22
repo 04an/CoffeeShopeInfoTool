28a1f3eaec03539c6a63c6c55b19d701164a7efd
04an <olegdzhum@gmail.com>
2020-04-22 13:27:25 +0200

    Add project

---
+++ CoffeeHouse.ShopInfoTool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoffeeHouse.ShopInfoTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Coffee House - Shop info toll!");
+        }
+    }
+}
